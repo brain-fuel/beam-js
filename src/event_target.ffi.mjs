fc040dbbd7bec5738791f41c0d0ddca585f7a7a9
brain-fuel <84283509+brain-fuel@users.noreply.github.com>
2025-05-23 16:14:11 -0400

    Add parameter and return types to JSDoc

--- src/event_target.ffi.mjs
+++ src/event_target.ffi.mjs
@@ -1,20 +1,53 @@
+/**
+ * Calls `window.addEventListener()` from the global context.
+ * @param {...any} args
+ * @returns {void}
+ */
 export function addEventListener(...args) {
   return (globalThis).addEventListener(...args);
 }
+/**
+ * Calls `target.addEventListener()` on the provided target.
+ * @param {EventTarget} target
+ * @param {...any} args
+ * @returns {void}
+ */
 export function addEventListenerTo(target, ...args) {
   return (target).addEventListener(...args);
 }
 
+/**
+ * Calls `globalThis.dispatchEvent()` from the global context.
+ * @param {...any} args
+ * @returns {boolean}
+ */
 export function dispatchEvent(...args) {
   return (globalThis).dispatchEvent(...args);
 }
+/**
+ * Calls `target.dispatchEvent()` on the provided target.
+ * @param {EventTarget} target
+ * @param {...any} args
+ * @returns {boolean}
+ */
 export function dispatchEventTo(target, ...args) {
   return (target).dispatchEvent(...args);
 }
 
+/**
+ * Calls `globalThis.removeEventListener()` from the global context.
+ * @param {...any} args
+ * @returns {void}
+ */
 export function removeEventListener(...args) {
   return (globalThis).removeEventListener(...args);
 }
+/**
+ * Calls `target.removeEventListener()` on the provided target.
+ * @param {EventTarget} target
+ * @param {...any} args
+ * @returns {void}
+ */
 export function removeEventListenerFrom(target, ...args) {
   return (target).removeEventListener(...args);
 }
